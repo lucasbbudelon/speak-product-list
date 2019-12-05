ad8c353c37a99e62b82c56784470a3a550e35e93
lucasbbudelon <lucasbbudelon@gmail.com>
2019-12-05 10:31:07 -0200

    set const list products

---
+++ src/app/services/products/products.constants.ts
@@ -0,0 +1,52 @@
+import { Product } from './products.model';
+
+export const PRODUCTS_LIST: Product[] = [
+    {
+        id: '1',
+        name: 'Leite',
+        description: 'UHT integral',
+        price: 2.89
+    },
+    {
+        id: '2',
+        name: 'Hamburguer',
+        description: 'Carne bovina moída congelada',
+        price: 6.99
+    },
+    {
+        id: '3',
+        name: 'Mistura para bolo',
+        description: 'Sabor chocolate',
+        price: 2.25
+    },
+    {
+        id: '4',
+        name: 'Cereal',
+        description: 'Flakes',
+        price: 3.99
+    },
+    {
+        id: '5',
+        name: 'Lampada',
+        description: 'Classic, 100 watt, 220v',
+        price: 4.99
+    },
+    {
+        id: '6',
+        name: 'Pó para preparo de sorvete',
+        description: 'Sabor morango',
+        price: 12.5
+    },
+    {
+        id: '7',
+        name: 'Ketchup',
+        description: 'Tomasito tradicional',
+        price: 3.1
+    },
+    {
+        id: '8',
+        name: 'Detergente',
+        description: 'Limpo tradicional',
+        price: 1.89
+    }
+];
--- src/app/services/products/products.service.ts
+++ src/app/services/products/products.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 import { Product } from './products.model';
+import { Observable, of } from 'rxjs';
+import { PRODUCTS_LIST } from './products.constants';
 
 @Injectable({
   providedIn: 'root'
@@ -13,13 +15,15 @@ export class ProductsService {
     private httpClient: HttpClient,
   ) { }
 
-  getAll() {
-    const url = `${environment.api}/products`;
-    return this.httpClient.get<Product[]>(url);
+  getAll(): Observable<Product[]> {
+    // const url = `${environment.api}/products`;
+    // return this.httpClient.get<Product[]>(url);
+    return of(PRODUCTS_LIST);
   }
 
-  getById(id: number) {
-    const url = `${environment.api}/products/${id}`;
-    return this.httpClient.get<Product>(url);
+  getById(id: number): Observable<Product> {
+    // const url = `${environment.api}/products/${id}`;
+    // return this.httpClient.get<Product>(url);
+    return of(PRODUCTS_LIST.find(p => p.id === id));
   }
 }
